0f28627fcc65a7ca8b07d802172f3a6bb2105af6
William Heng <williamheng1993@gmail.com>
2016-07-30 09:16:14 +0100

    Missed out a block when writing FT, fix FT errors

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -61,6 +61,12 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
         # Francis starts a new list by entering a new item. He is less interesting than Edith
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
@@ -68,7 +74,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
-        self.assertEqual(francis_list_url, '/lists/.+')
+        self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, there is no trace of Edith's list

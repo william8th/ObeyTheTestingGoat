cfd554ee53a86c8bb515c895759445e015fabdf2
William Heng <williamheng1993@gmail.com>
2016-07-30 09:36:34 +0100

    Add URL to create new list, moved code in views and tests
    Why:
    - POST request to /lists should create a new list
    - Home page no longer needs to create the list (separation of responsibility)

--- lists/tests.py
+++ lists/tests.py
@@ -20,32 +20,24 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
+class NewListTest(TestCase):
 
-        response = home_page(request)
-
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            '/lists',
+            data={'item_text': 'A new list item'}
+        )
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
 
-    def test_home_page_redirects_after_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            '/lists',
+            data={'item_text': 'A new list item'}
+        )
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
 class ItemModelTest(TestCase):

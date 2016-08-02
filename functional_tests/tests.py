3cb815f8c11fc0521ac36e15877519202c0068f6
William Heng <williamheng1993@gmail.com>
2016-08-02 21:36:41 +0100

    Use Bootstrap to improve layout
    Why:
    - We are lazy developers
    - We need to keep the UI good
    - Thanks Twitter!

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -102,7 +102,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('testing\n')
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.siz['width'] / 2,
+            inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
             delta=5
         )
